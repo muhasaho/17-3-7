4eb78a63dbb0dfeb217286ee85a6c43a79032023
Muhammed Saho <muhasaho@gmail.com>
2017-03-08 21:58:11 -0600

    more deployment fixes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -25,7 +25,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'The future of statistics starts here...'
+      msg: 'Try again'
     }
   }
 }
--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -6,6 +6,6 @@ describe('Hello.vue', () => {
     const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('The future of statistics starts here...')
+      .to.equal('Try again')
   })
 })
